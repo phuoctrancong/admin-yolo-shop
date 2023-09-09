cff91dbe947d1d103899f5c649c21c6d747480db
phuoctrancong <phuoc.trancong@vti.com.vn>
2023-09-09 17:39:23 +0700

    fix big

--- src/constants/endpoint.js
+++ src/constants/endpoint.js
@@ -1,53 +1,70 @@
+import {
+  InsertRowBelowOutlined,
+  ShoppingCartOutlined,
+  TeamOutlined,
+  NodeIndexOutlined,
+  CoffeeOutlined,
+  DollarOutlined,
+  AreaChartOutlined,
+} from "@ant-design/icons";
 export const Endpoint = {
-	HOME: '/',
-	DASHBOARD: '/dashboard',
-	BRANCH: '/branch',
-	SETTING: '/setting',
-	CATEGORY: '/categories',
-	COLOR: '/colors',
-	COUPON: '/coupon',
-	SPECIFICATION: '/specification',
-	PRODUCT: '/products',
-	USER: '/user',
-	ORDER: '/orders',
-	SIZE: '/sizes',
+  HOME: "/",
+  DASHBOARD: "/dashboard",
+  BRANCH: "/branch",
+  SETTING: "/setting",
+  CATEGORY: "/categories",
+  COLOR: "/colors",
+  COUPON: "/coupon",
+  SPECIFICATION: "/specification",
+  PRODUCT: "/products",
+  USER: "/user",
+  ORDER: "/orders",
+  SIZE: "/sizes",
 };
 
 export const routers = [
-	{
-		endpoint: Endpoint.DASHBOARD,
-		text: 'Dashboard',
-	},
-	{
-		endpoint: Endpoint.CATEGORY,
-		text: 'Quản lý danh mục',
-	},
-	{
-		endpoint: Endpoint.PRODUCT,
-		text: 'Quản lý sản phẩm',
-	},
-	{
-		endpoint: Endpoint.COLOR,
-		text: 'Quản lý màu sắc',
-	},
-	{
-		endpoint: Endpoint.SIZE,
-		text: 'Quản lý kích cỡ',
-	},
-	// {
-	//   endpoint: Endpoint.COUPON,
-	//   text: "Quản lý mã giảm giá",
-	// },
-	{
-		endpoint: Endpoint.USER,
-		text: 'Quản lý người dùng',
-	},
-	{
-		endpoint: Endpoint.ORDER,
-		text: 'Quản lý đơn hàng',
-	},
-	// {
-	//   endpoint: Endpoint.SETTING,
-	//   text: "Cài đặt website",
-	// },
+  {
+    endpoint: Endpoint.DASHBOARD,
+    text: "Dashboard",
+    icon: <AreaChartOutlined />,
+  },
+  {
+    endpoint: Endpoint.CATEGORY,
+    text: "Quản lý danh mục",
+    icon: <InsertRowBelowOutlined />,
+  },
+  {
+    endpoint: Endpoint.PRODUCT,
+    text: "Quản lý sản phẩm",
+    icon: <DollarOutlined />,
+  },
+  {
+    endpoint: Endpoint.COLOR,
+    text: "Quản lý màu sắc",
+    icon: <CoffeeOutlined />,
+  },
+  {
+    endpoint: Endpoint.SIZE,
+    text: "Quản lý kích cỡ",
+    icon: <NodeIndexOutlined />,
+  },
+  // {
+  //   endpoint: Endpoint.COUPON,
+  //   text: "Quản lý mã giảm giá",
+  // },
+  {
+    endpoint: Endpoint.USER,
+    text: "Quản lý người dùng",
+    icon: <TeamOutlined />,
+  },
+
+  {
+    endpoint: Endpoint.ORDER,
+    text: "Quản lý đơn hàng",
+    icon: <ShoppingCartOutlined />,
+  },
+  // {
+  //   endpoint: Endpoint.SETTING,
+  //   text: "Cài đặt website",
+  // },
 ];
